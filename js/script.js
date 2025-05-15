216e572d709bf415b2ab412baf9cebd74ccd94cf
Phil Kanter <phil.kanter@kingfisher.com>
2025-05-15 11:40:03 +0100

    Changing to 0.1 increments.

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@ function wheelFunction(sectors, buttonId, canvasId) {
   const arc = TAU / tot;
   function getRandomSteppedDecimal() {
     var steps = [];
-    for (var i = 0.45; i <= 0.95; i += 0.05) {
+    for (var i = 0.40; i <= 0.90; i += 0.10) {
       steps.push(parseFloat(i.toFixed(2)));
     }
     var random_index = Math.floor(Math.random() * steps.length);
